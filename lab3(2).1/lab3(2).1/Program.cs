a6d0562276aeb77e7f4e600f175dba3012447d8a
Anastasiia1720 <163123780+Anastasiia1720@users.noreply.github.com>
2024-10-01 23:19:46 +0300

    commit

---
+++ lab3(2).1/lab3(2).1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Задаємо масив цілих чисел
+        int[] numbers = { 5, 10, 15, 20, 25 };
+
+        // Обчислюємо суму всіх елементів масиву
+        int sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+
+        // Обчислюємо середнє арифметичне
+        double average = (double)sum / numbers.Length;
+
+        // Виводимо результат
+        Console.WriteLine("Середнє арифметичне: " + average);
+    }
+}
+
