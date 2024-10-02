14fbbb9d8f6653fcdea4dc4da899109001dfc855
Anastasiia1720 <163123780+Anastasiia1720@users.noreply.github.com>
2024-10-02 08:49:20 +0300

    commit

---
+++ lab3(2).4/lab3(2).4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Запитуємо користувача ввести кількість елементів масиву
+        Console.WriteLine("Введіть кількість елементів масиву:");
+        int length = int.Parse(Console.ReadLine());
+
+        // Створюємо масив і заповнюємо його введеними значеннями
+        int[] numbers = new int[length];
+        Console.WriteLine("Введіть елементи масиву:");
+        for (int i = 0; i < length; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        // Обчислюємо суму додатних елементів
+        int positiveSum = SumPositiveElements(numbers);
+
+        // Виводимо результат
+        Console.WriteLine("Сума всіх додатних елементів: " + positiveSum);
+    }
+
+    // Функція для обчислення суми додатних елементів
+    static int SumPositiveElements(int[] array)
+    {
+        int sum = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > 0)
+            {
+                sum += array[i];
+            }
+        }
+        return sum;
+    }
+}
+
