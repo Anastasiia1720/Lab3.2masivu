b805ee0ab67c451ad477abf3bc86ba5b3da97149
Anastasiia1720 <163123780+Anastasiia1720@users.noreply.github.com>
2024-10-02 08:46:19 +0300

    commit

---
+++ lab3(2).3/lab3(2).3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Запитуємо користувача ввести кількість елементів масиву
+        Console.WriteLine("Введіть кількість елементів масиву:");
+        int length = int.Parse(Console.ReadLine());
+
+        // Створюємо масив і заповнюємо його введеними значеннями
+        int[] numbers = new int[length];
+        Console.WriteLine("Введіть елементи масиву:");
+        for (int i = 0; i < length; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        // Виводимо всі парні числа
+        Console.WriteLine("Парні числа в масиві:");
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] % 2 == 0)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
